1d0cd103e52ed167ee03adef0d1799af1c920df6
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2012-12-09 18:14:06 +0100

    First exercises of learn c the hard way.

---
+++ ex01/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+	puts("Hello World!1");
+	puts("Hello World!2");
+	puts("Hello World!3");
+	puts("Hello World!4");
+	puts("Hello World!5");
+
+	return 0;
+}
