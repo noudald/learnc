08fcd003b7f7a8ff5599e33b728f2764026b2d42
Noud Aldenhoven <noud@walter.(none)>
2012-12-29 21:13:22 +0100

    Geen zin in opgaven 35... skip!

--- ex35/src/lcthw/darray_algos.h
+++ ex35/src/lcthw/darray_algos.h
@@ -6,7 +6,5 @@
 typedef int (*DArray_compare)(const void *a, const void *b);
 
 int DArray_qsort(DArray *array, DArray_compare cmp);
-int DArray_heapsort(DArray *array, DArray_compare cmp);
-int DArray_mergesort(DArray *array, DArray_compare cmp);
 
 #endif
